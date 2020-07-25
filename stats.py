857b70a5940a51a2d8077da43fca5ce970a92571
hjcian <chutarojian@gmail.com>
2020-07-25 11:11:48 +0800

    🔨 Refactor: Do some refinements and sugesstions

--- stats.py
+++ stats.py
@@ -1,21 +1,32 @@
 import sys 
-l1 =sys.argv
-l2 = []
-b = 0
-for i in range(1,len(l1)): #先將list 裡面的 str 轉成 int
-    l2.append(int(l1[i]))  #小問題 遇到字串會掛調 所以從1 開始
-l2.sort()    #對l2 進行排序 結果回傳到l2
-for i in range(0,len(l2)):
-    b += l2[i]
-c=b/len(l2)
 
+# filter user input
+user_input_nums = sys.argv[1:]
+for i, v in enumerate(user_input_nums):
+    user_input_nums[i] = int(v)
+user_input_nums.sort()
 
+# calc summation
+summation = 0
+for v in user_input_nums:
+    summation += v
+# or, 
+# >>> summation = sum(user_input_nums)
+# 既然都知道有 min() 與 max() 了，其實內建也有 sum() 啦~
 
-print("your input (in ascending order):",l2)
-print("sum------",b)
-print("mean------",c)
+# calc mean
+mean = summation / len(user_input_nums)
+
+print("your input (in ascending order):", user_input_nums)
+print("sum------", summation)
+print("mean------", mean)
 print("med-------") #還沒做
-print("min-------",min(l2)) 
-print("max-------",max(l2)) 
+print("min-------", min(user_input_nums))
+print("max-------", max(user_input_nums))
 
-
+'''
+修訂重點：
+1. 變數使用有含意的命名
+2. 逐漸拋棄使用 index 迭代物件的思維，若你確實需要 index 的資訊使用 enumerate() 最有效率
+3. 再熟悉一下 list slice 的用法，很方便
+'''
